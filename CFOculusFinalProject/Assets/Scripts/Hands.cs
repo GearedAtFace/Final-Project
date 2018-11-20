b24e29c1a5c238bd0ba356af73a5e0e44ad4f19b
GearedAtFace <37555402+GearedAtFace@users.noreply.github.com>
2018-11-20 11:50:55 -0500

    First Push

---
+++ CFOculusFinalProject/Assets/Scripts/Hands.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hands : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+
+    private void Update()
+    {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(controller);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(controller);
+    }
+}
