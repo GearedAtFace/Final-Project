0ae67d131cd6389c79b89f454c4ce4cec9f149c8
GearedAtFace <37555402+GearedAtFace@users.noreply.github.com>
2018-12-04 11:51:53 -0500

    day 3

---
+++ CFOculusFinalProject/Assets/Scripts/FireInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireInfo : MonoBehaviour
+{
+    public Vector3 spawnPos;
+    public GameObject bullet;
+}
--- CFOculusFinalProject/Assets/Scripts/Spawner.cs
+++ CFOculusFinalProject/Assets/Scripts/Spawner.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
     public Transform target;
-    public float fireRate = 0.1f;
-    public GameObject bullet;
+    public float fireRate = 0.25f;
+    public List<GameObject> spawnBullet;
+    public List<Vector3> spawnPos;
     private float fireTimer;
+    private int i = 0;
 
     private void Start()
     {
@@ -17,10 +19,14 @@ public class Spawner : MonoBehaviour {
     {
         fireTimer -= Time.deltaTime;
         if(target)transform.LookAt(target);
+
         if (fireTimer <= 0)
         {
-            Instantiate(bullet, transform.position, transform.rotation);
+            if(spawnBullet[i])Instantiate(spawnBullet[i], 
+                new Vector3(transform.position.x + (spawnPos[i].x/10), transform.position.y + (spawnPos[i].y/10), transform.position.z),
+                transform.rotation);
             fireTimer = fireRate;
+            i++;
         }
 	}
 }
